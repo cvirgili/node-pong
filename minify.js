b05d67a74271a555dfd3a2c48511e3f0f5f22a3b
cvirgili <cristiano.virgili@gmail.com>
2017-11-13 22:51:25 +0100

    ok

--- minify.js
+++ minify.js
@@ -18,3 +18,12 @@ compressor.minify({
             console.log(err);
     }
 });
+compressor.minify({
+    compressor: 'gcc',
+    input: __dirname + '/contents/javascript/fb.login.js',
+    output: __dirname + '/contents/javascript/fb.login.min.js',
+    callback: function(err, min) {
+        if (err)
+            console.log(err);
+    }
+});
