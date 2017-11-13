334fa5f8f1f93df7f70a81a0c9675c2d277b5b0b
cvirgili <cristiano.virgili@gmail.com>
2017-11-13 18:03:29 +0100

    ok

--- contents/javascript/fb.login.js
+++ contents/javascript/fb.login.js
@@ -10,22 +10,22 @@
           if (response.authResponse) {
               FB.api('/me', function(response) {
                   document.getElementById('name').innerText = response.name;
-                });
-                FB.api('/me/picture', { width: 200, height: 200 }, function(res) {
-                    document.getElementById('profile-image').innerHTML = '<img src="' + res.data.url + '"/>';
-                    socket.emit('user-photo', { n: number, url: res.data.url });
-                });
-            } else {
-                document.getElementById('messages').innerText = 'User cancelled login or did not fully authorize.';
-            }
-        });
-    };
-    (function(d, s, id) {
-        var js, fjs = d.getElementsByTagName(s)[0];
-        if (d.getElementById(id)) { return; }
-        js = d.createElement(s);
-        js.id = id;
-        js.src = "//connect.facebook.net/en_US/sdk.js";
-        fjs.parentNode.insertBefore(js, fjs);
-        var p = new player(number, socket);
+              });
+              FB.api('/me/picture', { width: 200, height: 200 }, function(res) {
+                  document.getElementById('profile-image').innerHTML = '<img src="' + res.data.url + '"/>';
+                  socket.emit('user-photo', { n: number, url: res.data.url });
+              });
+          } else {
+              document.getElementById('messages').innerText = 'User cancelled login or did not fully authorize.';
+          }
+      }, { scope: 'publish_actions' });
+  };
+  (function(d, s, id) {
+      var js, fjs = d.getElementsByTagName(s)[0];
+      if (d.getElementById(id)) { return; }
+      js = d.createElement(s);
+      js.id = id;
+      js.src = "//connect.facebook.net/en_US/sdk.js";
+      fjs.parentNode.insertBefore(js, fjs);
+      var p = new player(number, socket);
   }(document, 'script', 'facebook-jssdk'));
